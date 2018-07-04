8b36c6a65f1ae846c35494a319d82aed73ef3355
natee1992 <1092803008@qq.com>
2018-07-04 16:31:59 +0800

    注册返回token

--- shiyu/Api/signals.py
+++ shiyu/Api/signals.py
@@ -1,1 +1,12 @@
 # '2018/7/4 14:30'
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        # Token.objects.create(user=instance)
+        pass
--- shiyu/Api/views.py
+++ shiyu/Api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from .serializers import ShiYuSerializer, KeySerializer, UserRegisterSerializer
 from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from Notes.models import ShiYu
 from django.contrib.auth.models import User
+from rest_framework_jwt.serializers import jwt_encode_handler,jwt_payload_handler
 
 
 class ShiYuViewSets(CreateModelMixin, viewsets.GenericViewSet):
@@ -23,4 +25,17 @@ class UserViewsets(CreateModelMixin, viewsets.GenericViewSet):
     '''
     queryset = User.objects.all()
     serializer_class = UserRegisterSerializer
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        #生成token
+        user = self.perform_create(serializer)
+        re_dict = serializer.data
+        payload = jwt_payload_handler(user)
+        re_dict['token'] = jwt_encode_handler(payload)
+        headers = self.get_success_headers(serializer.data)
+        return Response(re_dict, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        return serializer.save()
 
